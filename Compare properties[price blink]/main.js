3bf03a7d32c734e21d5ba0cd9e293cd485160480
govindrao54 <govindrao.kulkarni@proptiger.com>
2016-09-03 04:53:57 +0530

    removes similar proj components

--- Compare properties[price blink]/main.js
+++ Compare properties[price blink]/main.js
@@ -26,7 +26,6 @@ var coupon_code_exception_rids = [];
 var DEBUG = false;
 var debug_info = {};
 var getProjectDetailsUrl = 'https://www.makaan.com/petra/app/v4/project-detail';
-// var getSimilarProjectsUrl = 'https://www.makaan.com/petra/data/v2/recommendation?type=similar&projectId=642535&selector={"fields":["title","builder","activeStatus","projectId","URL","buyUrl","rentUrl","imageURL","altText","mainImage","minPrice","maxPrice","minResaleOrPrimaryPrice","maxResaleOrPrimaryPrice","id","city","suburb","label","name","type","user","contactNumbers","locality","contactNumber","sellerId","listingCategory","property","currentListingPrice","price","bedrooms","bathrooms","size","unitTypeId","project","projectId","studyRoom","servantRoom","poojaRoom","companySeller","company","companyScore","listingAggregations"],"paging":{"start":0,"rows":10}}&sourceDomain=Makaan';
 
 function getProjectDetails(projectIds) {
     var httpRequest = new XMLHttpRequest();
@@ -83,7 +82,6 @@ function alertContents() {
             });
             if (projectIds.length > 0) {
                 getProjectDetails(projectIds);
-                getSimilarProjects(projectIds);
             } else {
                 console.log("No Project found")
             }
